5d81a5b1a910315883ffbfdb6d00e0d37714632d
eemelihayrynen <55876843+eemelihayrynen@users.noreply.github.com>
2023-04-03 01:33:27 +0300

    Added some tests

--- database/my_test.py
+++ database/my_test.py
@@ -1,9 +1,9 @@
+import json
 import os
 import pytest
 import tempfile
-import app
+from app import *
 from app import Movie, Actor, Director, StreamingService
-
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
 
@@ -14,18 +14,37 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.close()
 
 @pytest.fixture
-def db_handle():
-    print("ass")
+def client():
     db_fd, db_fname = tempfile.mkstemp()
-    app.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_fname
-    app.app.config["TESTING"] = True
-    with app.app.app_context():
-        app.db.create_all()
-    yield app.db
-    app.db.session.remove()
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_fname
+    app.config["TESTING"] = True
+
+    db.create_all()
+    _populate_db()
+
+    yield app.test_client()
+
+    db.session.remove()
     os.close(db_fd)
     os.unlink(db_fname)
 
+def _populate_db():
+    for i in range(1,3):
+        movie = Movie(title="movie {}".format(i), comments="comments", rating=i, writer="writer", release_year=i, genres="action", actors=[])
+        db.session.add(movie)
+        actor = Actor(first_name = "Daniel", last_name=str(i))
+        db.session.add(actor)
+    db.session.commit()
+
+"""
+
+def test_add_film(client):
+    movie = _get_film()
+    client.session.add(movie)
+    client.session.commit()
+    print("success")
+    assert Movie.query.count() == 1
+"""
 def _get_film():
     title= "The Dark Knight"
     comments= ""
@@ -33,14 +52,84 @@ def _get_film():
     writer= "David S. Goyer"
     release_year= 2008
     genres= "Action/Drama/Crime"
-    return Movie(
-            title=title, comments=comments, rating=rating, writer=writer, release_year=release_year, genres=genres, actors=[]
-            )
+    directors = "Chris Nolan"
+    streamingServices = "HBO Max"
+    return {"title": title, "comments":comments, "rating":rating, "writer":writer, "release_year":release_year, "genres":genres, "actors":[{"first_name":"Daniel","last_name":"Craig"}]
+            ,"directors":[{"first_name":"Daniel","last_name":"Craig"}], "streaming_services": [{"name": "Netflix"}] }
 
-def test_add_film(db_handle):
-    movie = _get_film()
-    db_handle.session.add(movie)
-    db_handle.session.commit()
-    print("success")
-    assert Movie.query.count() == 1
-    
+class Testing(object):
+    MOVIE_URL = "/movie/movie 1/"
+    ACTOR_URL = "/actor/Daniel 1/"
+    MOVIE_POST_URL = "/movie/"
+    ACTOR_POST_URL = "/actor/"
+    MOVIE_URL2 = "/movie/movie 2/"
+    ACTOR_URL2 = "/actor/Daniel 2/"
+    def test_get_film(self,client):
+        """
+        tests get method for movie, returns 200 when correct also tests the length of the data
+        """
+        resp = client.get(self.MOVIE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert len(body) == 9 #there are 9 categories of data in a movie
+        assert body["title"]
+
+    def test_delete_film(self,client):
+        """
+        tests delete method for movie, returns 200 when correct and also tests after if movie is really gone and returns not found 404
+        """
+        resp = client.delete(self.MOVIE_URL)
+        assert resp.status_code == 200
+        resp = client.get(self.MOVIE_URL)
+        assert resp.status_code == 404
+
+    def test_get_actor(self,client):
+        """
+        tests get method for actor, returns 200 when correct also tests the length of the data
+        """
+        resp = client.get(self.ACTOR_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert len(body) == 2 #there are 2 categories of data in a actor
+        assert body["first_name"]
+
+    def test_delete_actor(self,client):
+        """
+        tests delete method for actor, returns 200 when correct and also tests after if actor is really gone and returns not found 404
+        """
+        resp = client.delete(self.MOVIE_URL)
+        assert resp.status_code == 200
+        resp = client.get(self.MOVIE_URL)
+        assert resp.status_code == 404
+
+    def test_add_movie(self,client):
+        """
+        tests post method for movie
+        """
+        m = _get_film()
+        resp = client.post(self.MOVIE_POST_URL,json = m)
+        assert resp.status_code == 201
+
+    def test_add_actor(self,client):
+        """
+        tests post method for actor
+        """
+        a = {"first_name":"Daniel","last_name":"Craig"}
+        resp = client.post(self.ACTOR_POST_URL,json = a)
+        assert resp.status_code == 201
+
+    def test_edit_movie(self,client):
+        """
+        tests put method for movie
+        """
+        m = _get_film()
+        resp = client.post(self.MOVIE_URL2,json = m)
+        assert resp.status_code == 204
+
+    def test_edit_actor(self,client):
+        """
+        tests put method for movie
+        """
+        a = {"first_name":"Daniel","last_name":"Craig"}
+        resp = client.post(self.MOVIE_URL2,json = a)
+        assert resp.status_code == 204
